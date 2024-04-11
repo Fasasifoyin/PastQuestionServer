a991c578d33bb95715f2061ae62d7aff55893af8
foyinjohn <fasasifoyin@gmail.com>
2024-04-10 14:23:54 -1000

    second commit

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ import question from "./routes/Question.js";
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "localhost:5173",
+      "https://pastquestion.vercel.app/",
+    ],
+  })
+);
 
 // app.use("/api/import", sendQuestion);
 app.use("/api", question);
